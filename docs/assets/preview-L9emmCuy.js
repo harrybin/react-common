e0042b98983a0108fa6c500a8370f8a04d71529e
Harald Binkle <15869006+harrybin@users.noreply.github.com>
2023-12-20 07:49:41 +0100

    updated docs to include latest changes

---
+++ docs/assets/preview-L9emmCuy.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-e1X6Q8KH.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ZGEfTy5X.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-ZGEfTy5X.js","./chunk-HLWAVYOI-bQp9_oJV.js","./iframe-e1X6Q8KH.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./chunk-ZGA76URP-Uk8hDnuP.js","./extends-E30VzY7q.js","./index-ogXoivrg.js","./inheritsLoose-N4InywQs.js","./index-MVbLLYTZ.js","./index.modern-l2jW9lKX.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
